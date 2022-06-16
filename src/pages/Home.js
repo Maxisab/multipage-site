e0b90aa00cd893ab58d162a47d8d58d406b81d77
Max Sabate <max.sabate@gmail.com>
2022-06-16 16:47:00 -0700

    setup useFetch hook to load data to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
+import { useFetch } from '../hooks/useFetch'
+
+//styles
+import './Home.css'
+
 export default function Home() {
+  const { data: articles, isPending, error } = useFetch('http://localhost:3000/articles')
+  
   return (
-    <div>
-      <h2>Homepage</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit porro commodi, nemo iure maiores exercitationem ipsa eveniet cumque quasi placeat illum modi? Veritatis tenetur vel iure adipisci delectus saepe ab?</p>
+    <div className='home'>
+      <h2>Articles</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {articles && articles.map((article) => (
+        <div key={article.id} className='card'>
+          <h3>{article.title}</h3>
+          <p>{article.author}</p>
+        </div>
+      ))}
     </div>
   )
 }
