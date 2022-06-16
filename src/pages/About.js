c46825928d0f8606d5f138d1752b4e8533773543
Max Sabate <max.sabate@gmail.com>
2022-06-16 16:18:56 -0700

    setup react route (5.1) and switch

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit porro commodi, nemo iure maiores exercitationem ipsa eveniet cumque quasi placeat illum modi? Veritatis tenetur vel iure adipisci delectus saepe ab?</p>
+    </div>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit porro commodi, nemo iure maiores exercitationem ipsa eveniet cumque quasi placeat illum modi? Veritatis tenetur vel iure adipisci delectus saepe ab?</p>
+    </div>
+  )
+}
