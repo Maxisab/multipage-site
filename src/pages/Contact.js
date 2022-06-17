fb1e816986d4547e236fd7ef29ef7f8e86720a84
Max Sabate <max.sabate@gmail.com>
2022-06-16 17:21:52 -0700

    added useLocation for search params (not complete)

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,14 @@
+import { useLocation } from 'react-router-dom'
+ 
 export default function Contact() {
+  const queryString = useLocation().search
+
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name")
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here...</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit porro commodi, nemo iure maiores exercitationem ipsa eveniet cumque quasi placeat illum modi? Veritatis tenetur vel iure adipisci delectus saepe ab?</p>
     </div>
   )
