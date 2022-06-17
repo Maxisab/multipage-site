74a626952bd8e16782315e355b3aed0d8bc7ca44
Max Sabate <max.sabate@gmail.com>
2022-06-16 17:11:50 -0700

    setup useHistory to redirect in case of error

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,15 +1,27 @@
-import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
 import { useFetch } from '../hooks/useFetch'
 
 export default function Article() {
 const { id } = useParams()
 const url = 'http://localhost:3000/articles/' + id
 const { data: article, isPending, error } = useFetch(url)
+const history = useHistory()
+
+useEffect(() => {
+  if (error) {
+    // redirect
+    // history.goBack()
+    setTimeout(() => {
+      history.push('/')
+    }, 3000)
+  }
+}, [error, history])
 
   return (
     <div>
       {isPending && <div>Loading...</div>}
-      {error && <div>{error}</div>}
+      {error && <div>{error}. Redirecting to Home page...</div>}
       {article && (
         <div>
           <h2>{article.title}</h2>
